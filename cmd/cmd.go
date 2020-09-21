cfda922036f6ca78794812651c337da3d49d989d
dn200891uer2 <egor.uralov@privatbank.ua>
2020-09-21 20:29:58 +0300

    update cmd

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,1 +1,45 @@
-package cmd
+package main
+
+import (
+	"github.com/cat-in-vacuum/middleware_task/api"
+	"github.com/cat-in-vacuum/middleware_task/notificator"
+	"github.com/cat-in-vacuum/middleware_task/service"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//b := new(service.Box)
+
+	// b.Notificator = notificator.New(&http.Client{})
+
+	/*notifications := []service.Task{
+		service.Task{
+			URL: "example.com",
+		},
+		service.Task{
+			URL: "example.com",
+		},
+		service.Task{
+			URL: "example.com",
+		},
+		service.Task{
+			URL: "example.com",
+		},
+		service.Task{
+			URL: "example.com",
+		},
+		service.Task{
+			URL: "example.com",
+		},
+	}*/
+
+	// out := b.ProcessNotifications(context.TODO(), notifications)
+	n := notificator.New(http.DefaultClient)
+	box := &service.Box{
+		Notificator: n,
+	}
+	server := api.New(":8080", box)
+	log.Fatal(server.Start())
+	// b.ProcessNotifications(context.TODO(), notifications)
+}
