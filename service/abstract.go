05925f9198a0b0cb10c30974d91259a558b4faff
dn200891uer2 <egor.uralov@privatbank.ua>
2020-09-22 21:42:57 +0300

    log implemented

--- service/abstract.go
+++ service/abstract.go
@@ -3,9 +3,7 @@ package service
 import (
 	"context"
 	"github.com/cat-in-vacuum/middleware_task/notificator"
-	"sync"
 )
-
 
 // describes an entity that provides the ability to send notifications
 type Notificator interface {
