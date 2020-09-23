962845d0ad61cc4c85bf6675cb19f8889e19d12e
dn200891uer2 <egor.uralov@privatbank.ua>
2020-09-23 22:11:52 +0300

    logging middleware

--- api/middleware.go
+++ api/middleware.go
@@ -1,14 +1,22 @@
 package api
 
 import (
+	"github.com/cat-in-vacuum/middleware_task/log"
 	"net/http"
+	"time"
 )
 
-func rateLimiter() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
-		}
-		return http.HandlerFunc(fn)
-	}
+func rateLimiter(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+	})
+}
+
+func logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tn := time.Now()
+		next.ServeHTTP(w, r)
+		reqDur := time.Since(tn)
+		log.DebugHttpReq(r, reqDur)
+	})
 }
