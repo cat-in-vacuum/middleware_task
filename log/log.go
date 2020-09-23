069e64f68a399e5b239a96d81175aea88349371b
dn200891uer2 <egor.uralov@privatbank.ua>
2020-09-23 22:15:05 +0300

    update log

--- log/log.go
+++ log/log.go
@@ -1,8 +1,11 @@
 package log
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"runtime"
+	"time"
 )
 
 func Error(err error) {
@@ -13,4 +16,16 @@ func Error(err error) {
 func Debug(message interface{}) {
 	pc, fn, line, _ := runtime.Caller(1)
 	log.Printf("[debug] in [%s:%d] %s; %v", fn, line, runtime.FuncForPC(pc).Name(), message)
+}
+
+func Fatal(v  ...interface{}) {
+	log.Fatal(v)
+}
+
+func DebugHttpReq(r *http.Request, dur time.Duration) {
+	reqPath := r.URL.Path
+	method := r.Method
+	addr := r.RemoteAddr
+	logMsg := fmt.Sprintf("reqPath %s; reqMethod %s; addr: %s; duration: %s", reqPath, method, addr, dur.String())
+	Debug(logMsg)
 }
